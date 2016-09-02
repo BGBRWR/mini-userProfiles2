f7886b5da2f3bed424ed8a746448ff355e5ca8f6
Austin Walker <Austin Walker>
2016-09-02 16:10:35 -0600

    W3D3 Mini-project

--- controller.js
+++ controller.js
@@ -1,10 +1,28 @@
 angular.module('userProfiles').controller('MainController', function($scope, mainService) {
 
-  $scope.getUsers = function() {
-  	$scope.users = mainService.getUsers();
-  }
+  $scope.pageNum = 1;
 
 
+
+  $scope.getUsers = function () {
+    var promise = mainService.getUsers($scope.pageNum);
+    	promise.then(function(response) {
+        $scope.users = response.data.data;
+        $scope.totalPages = response.data.total_pages;
+      });
+  };
   $scope.getUsers();
+
+  $scope.prev = function () {
+    if ($scope.pageNum > 1) {
+      $scope.pageNum--;
+      $scope.getUsers();
+  }};
+
+  $scope.next = function() {
+    if ($scope.pageNum < $scope.totalPages) {
+      $scope.pageNum++;
+      $scope.getUsers();
+  }};
 
 });
